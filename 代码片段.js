8eb2e70aa2bd5662a1702337a3386bc2fca8769c
xujq <416474849@qq.com>
2020-03-30 22:53:17 +0800

    no message

--- 代码片段.js
+++ 代码片段.js
@@ -1,8 +1,8 @@
-var someResource = getData();
-setInterval(function() {
-    var node = document.getElementById('Node');
-    if(node) {
-        // 处理 node 和 someResource
-        node.innerHTML = JSON.stringify(someResource))
-    }
-}, 1000)
+function add(a, b) { 
+  a++ 
+  var c = a + b 
+  return c 
+  }
+  var m = 0,n = 19 // 把 m,n,add() 标记为进入环境。
+  add(m, n) // 把 a, b, c标记为进入环境。 
+  console.log(n) // a,b,c标记为离开环境，等待垃圾回收
